6a8a51d8706a7720e2b9e8048c8dd04b88671779
John Wilkes <jonathan2@Jonathans-MacBook-Pro.local>
2014-03-31 20:15:52 -0500

    Update entity to conform with latest state diagram

--- Model/elevator_arc_behave.vhd
+++ Model/elevator_arc_behave.vhd
@@ -1,3 +1,7 @@
+-- Elevator Controller
+-- CPE 526
+-- Taixing Bi (Hunter), Wesley Eledui, Justin Gay, John Wilkes
+
 architecture behavioral of elevator is
 begin
 
--- Model/elevator_entity.vhd
+++ Model/elevator_entity.vhd
@@ -1,8 +1,13 @@
+-- Elevator Controller
+-- CPE 526
+-- Taixing Bi (Hunter), Wesley Eledui, Justin Gay, John Wilkes
+
 Library IEEE;
 use ieee.std_logic_1164.all;
 
 entity elevator is
-	port(U1, U2, D2, D3, F1, F2, F3, DC, FS : in std_logic;
+	port(U1, U2, D2, D3, F1, F2, F3, DC : in std_logic;
+			 FS : in std_logic_vector(1 downto 0);
 			 door : out std_logic;
 			 direction : out std_logic_vector(1 downto 0));
 end elevator;
