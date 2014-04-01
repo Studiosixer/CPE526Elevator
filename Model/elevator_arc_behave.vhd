6b828562ee669c4c4e6f5d73c6c9cc72809b9e64
John Wilkes <jonathan2@Jonathans-MacBook-Pro.local>
2014-03-31 23:23:58 -0500

    Update outputs based on state
    We still need to update the state based on inputs

--- Model/elevator_arc_behave.vhd
+++ Model/elevator_arc_behave.vhd
@@ -3,6 +3,50 @@
 -- Taixing Bi (Hunter), Wesley Eledui, Justin Gay, John Wilkes
 
 architecture behavioral of elevator is
+	type stateType is (FloorStop1, FloorStop2, FloorStop3, CloseDoors1, CloseDoors2, CloseDoors3,
+										 Up1To2, Up2To3, Down3To2, Down2To1);
+	signal state : stateType;
 begin
+
+	process (clk)
+	begin
+		if (clk'event and clk = '1') then
+			-- TODO: change state based on inputs
+
+			-- update outputs based on state
+			case state is
+				when FloorStop1 =>
+					door <= '0';
+					direction <= "00";
+				when FloorStop2 =>
+					door <= '0';
+					direction <= "00";
+				when FloorStop3 =>
+					door <= '0';
+					direction <= "00";
+				when CloseDoors1 =>
+					door <= '1';
+					direction <= "00";
+				when CloseDoors2 =>
+					door <= '1';
+					direction <= "00";
+				when CloseDoors3 =>
+					door <= '1';
+					direction <= "00";
+				when Up1To2 =>
+					door <= '1';
+					direction <= "01";
+				when Up2To3 =>
+					door <= '1';
+					direction <= "01";
+				when Down3To2 =>
+					door <= '1';
+					direction <= "10";
+				when Down2To1 =>
+					door <= '1';
+					direction <= "10";
+			end case;
+		end if;
+	end process;
 
 end architecture;
--- Model/elevator_entity.vhd
+++ Model/elevator_entity.vhd
@@ -6,7 +6,33 @@ Library IEEE;
 use ieee.std_logic_1164.all;
 
 entity elevator is
-	port(U1, U2, D2, D3, F1, F2, F3, DC : in std_logic;
+	----- inputs -----
+	-- clk - clock
+	-- U1  - floor 1 up external button
+	-- U2  - floor 2 up external button
+	-- D2  - floor 2 down external button
+	-- D3  - floor 3 down external button
+	-- F1  - floor 1 internal button
+	-- F2	 - floor 2 internal button
+	-- F3  - floor 3 internal button
+	-- DC  - doors closed sensor
+	-- FS  - floor sensor
+	--   00 - invalid
+	--   01 - floor 1
+	--   10 - floor 2
+	--   11 - floor 3
+
+	----- outputs -----
+	-- door - open/close doors
+	-- 	 0 - open
+	--   1 - close
+	-- direction - elevator direction
+	--   00 - stationary
+	--   01 - up
+	--   10 - down
+	--   11 - invalid
+
+	port(clk, U1, U2, D2, D3, F1, F2, F3, DC : in std_logic;
 			 FS : in std_logic_vector(1 downto 0);
 			 door : out std_logic;
 			 direction : out std_logic_vector(1 downto 0));
