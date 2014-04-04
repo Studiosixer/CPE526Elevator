dec5b2e385c1399b68647ec1c6450c2e90f16e80
John Wilkes <jonathan2@Jonathans-MacBook-Pro.local>
2014-04-04 00:39:35 -0500

    Added state change based on inputs

--- Model/elevator_arc_behave.vhd
+++ Model/elevator_arc_behave.vhd
@@ -13,15 +13,69 @@ architecture behavioral of elevator is
 										 Up2To3,               -- Elevator is moving up from floor 2 to floor 3
 										 Down3To2,             -- Elevator is moving down from floor 3 to floor 2
 										 Down2To1);            -- Elevator is moving down from floor 2 to floor 1
-	signal state : stateType;
 begin
 
 	process (clk)
+		variable state : stateType;
 	begin
 		if (clk'event and clk = '1') then
-			-- TODO: change state based on inputs
+			-- change state based on inputs
+			case state is
+				when FloorStop1 =>
+					if (U2 = '1' or D2 = '1' or D3 = '1' or F2 = '1' or F3 = '1') then
+						state := WaitForClosedDoors1;
+					end if;
+				when FloorStop2 =>
+					if (U1 = '1' or D3 = '1' or F1 = '1' or F3 = '1') then
+						state := WaitForClosedDoors2;
+					end if;
+				when FloorStop3 =>
+					if (U1 = '1' or U2 = '1' or D2 = '1' or F1 = '1' or F2 = '1') then
+						state := WaitForClosedDoors3;
+					end if;
+				when WaitForClosedDoors1 =>
+					if (DC = '1') then
+						state := Up1To2;
+					end if;
+				when WaitForClosedDoors2 =>
+					if (DC = '1') then
+						if (D3 = '1' or F3 = '1') then
+							state := Up2To3;
+						elsif (U1 = '1' or F1 = '1') then
+							state := Down2To1;
+						end if;
+					end if;
+				when WaitForClosedDoors3 =>
+					if (DC = '1') then
+						state := Down3To2;
+					end if;
+				when Up1To2 =>
+					if (FS = "10") then
+						if (F2 = '1' or U2 = '1') then
+							state := FloorStop2;
+						else
+							state := Up2To3;
+						end if;
+					end if;
+				when Up2To3 =>
+					if (FS = "11") then
+						state := FloorStop3;
+					end if;
+				when Down3To2 =>
+					if (FS = "10") then
+						if (F2 = '1' or D2 = '1') then
+							state := FloorStop2;
+						else
+							state := Down2To1;
+						end if;
+					end if;
+				when Down2To1 =>
+					if (FS = "01") then
+						state := FloorStop1;
+					end if;
+			end case;
 
-			-- update outputs based on state
+			-- update outputs based on new state
 			case state is
 				when FloorStop1 =>
 					door <= '0';
