54e45421e5f8ad02a291bfa0970824fe7b22bab3
Wesley <wefilmer@gmail.com>
2014-04-17 09:01:43 -0500

    Adding new properties
    Adding new properties

--- Model/randomPacket.sv
+++ Model/randomPacket.sv
@@ -4,9 +4,18 @@
 
 package random;
 	class Packet;
-		rand int bttnPressTime, dcTime, travelTime;
+		rand int bttnPressTime,		//The amount of time a button is pressed
+						 dcTime,					//...?
+						 travelTime,			//...?
+						 timeBeforePress;	//Amount of time in clock cycles before the button is pressed again
 		constraint c {bttnPressTime >= 1; bttnPressTime <= 8;
 									dcTime >= 5; dcTime <= 20;
-									travelTime >= 10; travelTime <= 30;}
+									travelTime >= 10; travelTime <= 30;
+									timeBeforePress > 0; timeBeforePress <= 100;}
+
+		typedef enum {U1, U2, D2, D3, F1, F2, F3} buttons;
+		randc buttons b;
+		rand int buttonSelector;
+
 	endclass : Packet
 endpackage : random
